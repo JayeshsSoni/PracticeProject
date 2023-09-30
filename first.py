c1fce2dbd22c1c7400434fc4ba19990647fb9bff
sahil soni <sonijayeshkumar874@gmail.com>
2023-09-30 17:05:15 +0530

    first

---
+++ first.py
@@ -0,0 +1,11 @@
+#pyhton print function
+print("Hello World!")
+print(10+2)
+print(52>89)
+a = 85 
+b = 96 
+print(a+ 5)
+print(100-b)
+print(a+b)
+print(a-b+520)
+print("jk is my ", 80, "bag" ,2 ,"rs")
---
+++ varcheck.py
@@ -0,0 +1,8 @@
+a = 52
+c = 52
+b = a*c
+print(a)
+print(a+b)
+print(a-b)
+print(c)
+print(b)
